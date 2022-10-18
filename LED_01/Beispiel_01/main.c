14c7eff76e050ab764afc54acbf88055f87ed05c
m3ndax <adrian.goessl@outlook.com>
2022-10-18 21:50:39 +0200

    update code and comment

---
+++ LED_01/Beispiel_01/main.c
@@ -0,0 +1,23 @@
+/*
+ * Beispiel_01.c
+ *
+ * Created: 13.10.2022 16:01:01
+ * Author : Adrian
+ */ 
+#define F_CPU 12000000UL	//define the CPU frequency
+#include <avr/io.h>			//default lib AVR
+#include <util/delay.h>		//delay lib add delay time
+
+// main module
+int main(void) 
+{
+	DDRC=0xFF;  //LED-Port: Output
+	PORTC=0x00; //shut off all LED's(default)
+	while(1)
+	{
+		PORTC++;		//LED-Status increment
+		_delay_ms(500);	//wait 500ms 
+	}
+}
+
+
---
+++ LED_02/LED_02/main.c
@@ -0,0 +1,23 @@
+/*
+ * LED_02.c
+ *
+ * Created: 13.10.2022 22:20:28
+ * Author : Adrian
+ */ 
+
+#define F_CPU 12000000UL     //define the frequency
+#include <avr/io.h>			//default AVR lib with SFR regs and bit definitions
+#include <util/delay.h>     //lib for delay functions
+
+// main function
+  int main(void) 
+  {
+	  DDRC=0xFF;	//LED-Port (PortC): Output
+	  PORTC=0x55;	//LEDs 0,2,4,6 turn on
+	  while(1)		//start with loop
+	  {
+		  PORTC = ~PORTC;	//change all LED's
+		  _delay_ms(200);	//wait 200ms
+	  }						//end loop
+  } //end main
+
