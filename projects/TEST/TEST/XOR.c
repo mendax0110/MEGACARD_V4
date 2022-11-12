5df9e2ce0005c35210131aca1a5bd670c2bef0e8
m3ndax <adrian.goessl@outlook.com>
2022-11-12 20:21:00 +0100

    update xor file

---
+++ projects/TEST/TEST/XOR.c
@@ -0,0 +1,26 @@
+/*
+ * XOR.c
+ *
+ * Created: 12.11.2022 18:53:52
+ *  Author: Adrian
+ */ 
+#define F_CPU 12000000UL        //Taktfrequenz Quarz
+#include <avr/io.h>
+#include <util/delay.h>
+
+int main(void)
+{
+	/*XOR function with led ATMEGA 16*/
+
+	DDRC = 0xFF;            //Port B als Ausgang
+	PORTC = 0x00;           //Port B auf 0 setzen
+
+	while(1)
+	{
+		PORTC ^= 0xFF;      //Port B invertieren
+		_delay_ms(1000);    //1 Sekunde warten
+
+		PORTC = ~PORTC;     //Port B invertieren
+		_delay_ms(1000);    //1 Sekunde warten
+	}
+}
