16623a4561501c0f23d8fb123ce2efe855909bca
m3ndax <adrian.goessl@outlook.com>
2022-12-04 18:18:08 +0100

    update code and comment

--- projects/Voltmeter_2/Voltmeter_2/main.c
+++ projects/Voltmeter_2/Voltmeter_2/main.c
@@ -4,42 +4,75 @@
  * Created: 04.12.2022 13:02:56
  * Author : Adrian
  */ 
-
+#define F_CPU 12000000UL
 #include <avr/io.h>
 #include <util/delay.h>
-#include "lcd.h"  // Include the LCD driver
+
+#define LED_PORT PORTB
+#define LED_DDR DDRB
+
+// ADC input channel
+#define ADC_CHANNEL 0
+
+// Reference voltage (in volts)
+#define REF_VOLTAGE 5.0
+
+// Resolution of the ADC (in bits)
+#define ADC_RESOLUTION 10
+
+// Calculate the voltage conversion factor
+#define VOLT_FACTOR (REF_VOLTAGE / (1 << ADC_RESOLUTION))
+
+void adc_init(void)
+{
+	// Set the ADC input channel as an input
+	DDRB &= ~(1 << ADC_CHANNEL);
+
+	// Enable the ADC and set the clock prescaler
+	ADCSRA = (1 << ADEN) | (1 << ADPS2) | (1 << ADPS1);
+
+	// Set the ADC reference voltage to AVcc
+	ADMUX = (1 << REFS0);
+}
+
+// Read the ADC value on the specified channel
+uint16_t adc_read(uint8_t channel)
+{
+	// Set the ADC input channel
+	ADMUX = (ADMUX & 0xF0) | (channel & 0x0F);
+
+	// Start a single conversion
+	ADCSRA |= (1 << ADSC);
+
+	// Wait for the conversion to complete
+	while (ADCSRA & (1 << ADSC))
+	;
+
+	// Return the ADC value
+	return ADC;
+}
 
 int main(void)
 {
-	// Initialize the LCD screen
-	lcd_init();
+	// Initialize the ADC
+	adc_init();
 
-	// Set up the Atmega16A's ADC
-	ADMUX |= (1 << REFS0);  // Set the reference voltage to AVcc
-	ADCSRA |= (1 << ADPS2) | (1 << ADPS1) | (1 << ADPS0);  // Set the ADC clock prescaler to 128
-	ADCSRA |= (1 << ADEN);  // Enable the ADC
+	// Set the LED as an output
+	LED_DDR = 0xFF;
 
+	// Continuously read the ADC value and display it on the LED
 	while (1)
 	{
-		// Start a conversion on channel 0
-		ADMUX &= 0xF0;  // Clear the channel bits
-		ADMUX |= 0x00;  // Set the channel to 0
-		ADCSRA |= (1 << ADSC);  // Start the conversion
+		// Read the ADC value
+		uint16_t adc_value = adc_read(ADC_CHANNEL);
 
-		// Wait for the conversion to complete
-		while (ADCSRA & (1 << ADSC));
+		// Calculate the voltage
+		float voltage = adc_value * VOLT_FACTOR;
 
-		// Read the conversion result and display it on the LCD screen
-		uint16_t result = ADC;  // Read the result
+		// Display the voltage on the LED
+		LED_PORT = voltage;
 
-		char buffer[16];  // Create a buffer to hold the result as a string
-		snprintf(buffer, 16, "%d", result);  // Convert the result to a string
-
-		lcd_clear();  // Clear the LCD screen
-		lcd_puts(buffer);  // Display the result on the LCD screen
-
-		_delay_ms(500);  // Wait for half a second before starting the next conversion
+		// Wait for a short period of time
+		_delay_ms(100);
 	}
-
-	return 0;
 }
