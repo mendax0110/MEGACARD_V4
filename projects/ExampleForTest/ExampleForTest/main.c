b07f921d49796d49eb1338e27d30307b3368937d
m3ndax <adrian.goessl@outlook.com>
2022-12-26 11:02:22 +0100

    put everything in loop

--- projects/ExampleForTest/ExampleForTest/main.c
+++ projects/ExampleForTest/ExampleForTest/main.c
@@ -16,35 +16,34 @@ int main(void)
 	DDRC = 0xFF;
 	PORTC = 0x0F;
 	
-	_delay_ms(BLINK);
-	
-	PORTC |= 0xF0;	// OR-Gatter
-	
-	_delay_ms(BLINK);
-	
-	PORTC &= 0b10101010;	// AND-Gatter
-	
-	_delay_ms(BLINK);
-	
-	PORTC ^= 0b01010101;	// EXOR-Gatter
-	
-	_delay_ms(BLINK);
-	
-	PORTC =~ PORTC;			// Inverter
-	
-	_delay_ms(BLINK);
-	
-	PORTC =~(PORTC & 0b0101010101);	// NAND-Gatter
-	
-	_delay_ms(BLINK);
-	
-	PORTC =~(PORTC|0b11111111);		// NOR-Gatter
-	
-	_delay_ms(BLINK);
-	
 	
 	while(1)
 	{
+		_delay_ms(BLINK);
+		
+		PORTC |= 0xF0;	// OR-Gatter
+		
+		_delay_ms(BLINK);
+		
+		PORTC &= 0b10101010;	// AND-Gatter
+		
+		_delay_ms(BLINK);
+		
+		PORTC ^= 0b01010101;	// EXOR-Gatter
+		
+		_delay_ms(BLINK);
+		
+		PORTC =~ PORTC;			// Inverter
+		
+		_delay_ms(BLINK);
+		
+		PORTC =~(PORTC & 0b0101010101);	// NAND-Gatter
+		
+		_delay_ms(BLINK);
+		
+		PORTC =~(PORTC|0b11111111);		// NOR-Gatter
+		
+		_delay_ms(BLINK);
 	}
 }
 
