b0822cfd354b5f7110945b7f6a4b278d3f15f371
m3ndax <adrian.goessl@outlook.com>
2023-03-09 20:56:54 +0100

    simple timer/counter example

---
+++ projects/TimerCounter3/TimerCounter3/main.c
@@ -0,0 +1,41 @@
+/*
+ * TimerCounter3.c
+ *
+ * Created: 09.03.2023 15:51:41
+ * Author : Adrian
+ */ 
+#define F_CPU 12000000UL
+
+#include <avr/io.h>
+#include <avr/portpins.h>
+#include <avr/interrupt.h>
+
+
+void init_timer0()
+{
+	TCCR0 &= ~(1 << WGM00);							// Normal Mode
+	TCCR0 &= ~(1 << WGM01);	
+	TCCR0 |= (1 << COM00) | (1 << CS02) | (CS00);	// Toggle - OC0, F_CPU: 1024
+	TCNT0 = 245;									// Preload - 245 => Out = F_CPU:1024:10
+}
+
+void init_ports()
+{
+	DDRC = 0xFF;
+	PORTC |= (1 << PC7);	// Set LED Indicator
+	
+	DDRB |= (1 << PB3);		// OC0 - Ausgang Toggle
+}
+
+int main(void)
+{	
+    cli();			// lock	
+	init_ports();	// init the ports
+	init_timer0();	// init the timer
+	sei();			// enable global interrupts
+	
+    while (1) 
+    {
+    }
+}
+
