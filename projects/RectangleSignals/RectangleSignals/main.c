6427ffe7e4684414771ad046bd626ab840eb0316
m3ndax <adrian.goessl@outlook.com>
2023-04-18 22:10:13 +0200

    updated comments

--- projects/RectangleSignals/RectangleSignals/main.c
+++ projects/RectangleSignals/RectangleSignals/main.c
@@ -11,11 +11,13 @@
 #include <avr/interrupt.h>
 #include <util/delay.h>
 
+// Timer0 compare match interrupt service routine
 ISR(TIMER0_COMP_vect)
 {
 	PORTC ^= (1 << PC6); // Toggle PC6
 }
 
+// Initialize timer0 in CTC mode
 void init_timer0(uint8_t ocr_value)
 {
 	TCCR0 = (1 << WGM01) | (1 << COM00); // CTC mode, toggle OC0 on match
@@ -25,6 +27,7 @@ void init_timer0(uint8_t ocr_value)
 	TIMSK |= (1 << OCIE0); // Enable compare match interrupt
 }
 
+// Initialize ports
 void init_ports()
 {
 	DDRC = 0xFF;
@@ -33,6 +36,7 @@ void init_ports()
 	DDRB |= (1 << PB3); // OC0 output
 }
 
+// Main function
 int main(void)
 {
 	cli(); // Disable global interrupts
