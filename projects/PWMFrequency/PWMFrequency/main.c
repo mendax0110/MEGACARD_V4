c201dab865a920eb07afef8a6f4355ab1af96042
m3ndax <adrian.goessl@outlook.com>
2023-05-10 19:48:01 +0200

    updated comments

--- projects/PWMFrequency/PWMFrequency/main.c
+++ projects/PWMFrequency/PWMFrequency/main.c
@@ -4,50 +4,51 @@
  * Created: 06.05.2023 11:10:15
  * Author : Adrian
  */ 
-#define F_CPU 12000000UL
+#define F_CPU 12000000UL   // define the CPU frequency as 12 MHz
 
-#include <avr/io.h>
-#include <avr/interrupt.h>
-#include <avr/portpins.h>
+#include <avr/io.h>   // include AVR input/output definitions
+#include <avr/interrupt.h>   // include AVR interrupt definitions
+#include <avr/portpins.h>   // include AVR port pin definitions
 
-#include "ledPatterns/ledPatterns.h"
-#include "ledControl/ledControl.h"
-#include "InitFunctions/InitFunctions.h"
+#include "ledPatterns/ledPatterns.h"   // include the header file for LED patterns
+#include "ledControl/ledControl.h"   // include the header file for LED control
+#include "InitFunctions/InitFunctions.h"   // include the header file for initialization functions
 
 ISR(TIMER0_COMP_vect)
 {
-	// empty
+	// Interrupt Service Routine for the TIMER0 compare vector
+	// empty implementation
 }
 
 int main(void)
-{	
-	cli();    // disable global interrupts
+{
+	cli();   // disable global interrupts
 	
-	init_ports(); // call the InitFunctions
-	init_timer0_pwm();
-	sei();    // enable global interrupts
+	init_ports();   // initialize ports using the InitFunctions
+	init_timer0_pwm();   // initialize timer0 for PWM
+	sei();   // enable global interrupts
 
-	int pattern = 1;
-	int direction = 1;
+	int pattern = 1;   // variable to hold current pattern number
+	int direction = 1;   // variable to hold current direction of pattern
 
 	while (1)
 	{
-		change_pattern(&pattern, &direction);
+		change_pattern(&pattern, &direction);   // change the pattern and direction based on button input
 		
 		switch (pattern)
 		{
 			case 1:
-				led_pattern_1();
-				break;
+			led_pattern_1();   // display LED pattern 1
+			break;
 			case 2:
-				led_pattern_2();
-				break;
+			led_pattern_2();   // display LED pattern 2
+			break;
 			case 3:
-				led_pattern_3();
-				break;
+			led_pattern_3();   // display LED pattern 3
+			break;
 			default:
-			// invalid pattern number
-				break;
+			// invalid pattern number, do nothing
+			break;
 		}
 	}
 }
