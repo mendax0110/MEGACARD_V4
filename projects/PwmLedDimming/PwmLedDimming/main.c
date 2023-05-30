f68e92dfb7341e52d965067d7e8d91939b7952df
m3ndax <adrian.goessl@outlook.com>
2023-05-30 18:38:58 +0200

    added comments

--- projects/PwmLedDimming/PwmLedDimming/main.c
+++ projects/PwmLedDimming/PwmLedDimming/main.c
@@ -18,23 +18,23 @@ volatile uint8_t OCR0Var = 0;
 ISR(TIMER0_COMP_vect)
 {
 	OCR0 = OCR0Var;
-	PORTC |= (1 << PC4); // Schalte den Ausgang für die LED ein
+	PORTC |= (1 << PC4); // Schalte den Ausgang fÃ¼r die LED ein
 }
 
 // Timer 0 Overflow Interrupt Service Routine
 ISR(TIMER0_OVF_vect)
 {
-	PORTC &= ~(1 << PC4); // Schalte den Ausgang für die LED aus
+	PORTC &= ~(1 << PC4); // Schalte den Ausgang fÃ¼r die LED aus
 }
 
 // Funktion zur Initialisierung der Ports
 void init_Ports()
 {
-	DDRC = 0xFF; // Setze Port C als Ausgangsport für die LED
-	DDRB |= (1 << PB3); // Setze PB3 als Ausgangsport für den PWM-Ausgang
+	DDRC = 0xFF; // Setze Port C als Ausgangsport fÃ¼r die LED
+	DDRB |= (1 << PB3); // Setze PB3 als Ausgangsport fÃ¼r den PWM-Ausgang
 	
-	DDRA &= ~((1 << PA0)|(1 << PA1)); // Setze PA0 und PA1 als Eingänge
-	PORTA |= ((1 << PA0)|(1 << PA1)); // Aktiviere Pull-Up-Widerstände für PA0 und PA1
+	DDRA &= ~((1 << PA0)|(1 << PA1)); // Setze PA0 und PA1 als EingÃ¤nge
+	PORTA |= ((1 << PA0)|(1 << PA1)); // Aktiviere Pull-Up-WiderstÃ¤nde fÃ¼r PA0 und PA1
 }
 
 // Funktion zur Initialisierung des PWM
@@ -42,29 +42,29 @@ void init_PWM()
 {
 	TCCR0 |= (1 << WGM00)|(1 << WGM01)|(1 << CS00)|(1 << COM01); // Fast-PWM-Modus, Prescaler auf 1, non-inverting PWM
 	
-	OCR0 = OCR0Var; // Setze den Anfangswert für den PWM-Duty-Cycle
+	OCR0 = OCR0Var; // Setze den Anfangswert fÃ¼r den PWM-Duty-Cycle
 	
 	TIMSK |= (1 << TOIE0)|(1 << OCIE0); // Aktiviere Timer 0 Overflow- und Compare-Match-Interrupts
 }
 
 int main(void)
 {
-	cli(); // Deaktiviere Interrupts während der Initialisierung
-	init_Ports();
-	init_PWM();
+	cli(); // Deaktiviere Interrupts wÃ¤hrend der Initialisierung
+	init_Ports();	// Initialisiere die Ports
+	init_PWM();		// Initialisiere die PWM
 	sei(); // Aktiviere Interrupts
 	
 	while(1)
 	{
-		if((PINA & (1 << PC0)) == 0) // Überprüfe, ob der Taster an PA0 gedrückt ist
+		if((PINA & (1 << PC0)) == 0) // ÃœberprÃ¼fe, ob der Taster an PA0 gedrÃ¼ckt ist
 		{
-			OCR0Var += 1; // Erhöhe den PWM-Wert
-			_delay_ms(10); // Warte für eine kurze Zeit zur Entprellung
+			OCR0Var += 1; // ErhÃ¶he den PWM-Wert
+			_delay_ms(10); // Warte fÃ¼r eine kurze Zeit zur Entprellung
 		}
-		else if((PINA & (1 << PA1)) == 0) // Überprüfe, ob der Taster an PA1 gedrückt ist
+		else if((PINA & (1 << PA1)) == 0) // ÃœberprÃ¼fe, ob der Taster an PA1 gedrÃ¼ckt ist
 		{
 			OCR0Var -= 1; // Verringere den PWM-Wert
-			_delay_ms(10); // Warte für eine kurze Zeit zur Entprellung
+			_delay_ms(10); // Warte fÃ¼r eine kurze Zeit zur Entprellung
 		}
 		
 		if(OCR0Var >= 255) // Begrenze den PWM-Wert auf 253
