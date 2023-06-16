3cb94cdcb3a7c9f7bd0c18a6dfd66b7ed9704434
m3ndax <adrian.goessl@outlook.com>
2023-06-16 18:47:24 +0200

    added ISR and S0

--- projects/Timer1Zeitmessung/Timer1Zeitmessung/main.c
+++ projects/Timer1Zeitmessung/Timer1Zeitmessung/main.c
@@ -3,7 +3,8 @@
  *
  * Created: 15.06.2023 15:43:23
  * Author : Adrian
- */ 
+ */
+
 #define F_CPU 12000000UL
 
 #include <avr/io.h>
@@ -11,27 +12,46 @@
 #include <avr/portpins.h>
 #include <util/delay.h>
 
+long int ending_edge = 0;
+long int starting_edge = 0;
+long int clocks = 0;
+long int Time = 0;
+
+volatile long int Timer1Counter = 0;
+
 ISR(TIMER1_CAPT_vect)
 {
-		
+	ending_edge = 256 * ICR1H + ICR1L;
+	clocks = ending_edge - starting_edge + Timer1Counter * 65535;
+	Time = clocks / 1500; // Timer in ms (12Mhz/8) -> 1 500 000 / 1500 -> 1000Hz, das entspricht 1 ms
+	Timer1Counter = 0;
+	starting_edge = ending_edge;
+}
+
+ISR(TIMER0_OVF_vect)
+{
+	Timer1Counter++;
 }
 
 void initPorts()
 {
-	DDRC |= 0xFF;	// portc output
-	PORTC = 0xFF;	// all led dark
-		
-	DDRA &= ~(1 << PA0);	// for S0
-	PORTA |= (1 << PA0);	// for S0
+	DDRC |= 0xFF; // portc output
+	PORTC = 0xFF; // all led dark
+
+	DDRA &= ~(1 << PA0); // for S0
+	PORTA |= (1 << PA0); // for S0
+
+	DDRD &= ~(1 << PD6); // ICP Input Capture
+	PORTD &= ~(1 << PD6); // Hi-Z, cuz connected to PA0 (Pull up)
 	
-	DDRD &= ~(1 << PD6);	// ICP Input Capture
-	PORTD &= ~(1 << PD6);	// Hi-Z, cuz connected to PA0 (Pull up)
+	DDRC |= (1 << PC0);   // Set PC0 as output
+	PORTC |= (1 << PC0);  // Set PC0 high initially
 }
 
 void initTimer1()
 {
-	TCCR1A = 0x00;						// normal mode
-	TCCR1B |= (1 << ICNC1)|(1 << CS11);	// prescaler 8bit, noise canceller -> 1.5Mhz -> 0.66us -> Zeiteinheit	
+	TCCR1A = 0x00;                         // normal mode
+	TCCR1B |= (1 << ICNC1) | (1 << CS11);  // prescaler 8bit, noise canceller -> 1.5Mhz -> 0.66us -> Zeiteinheit
 }
 
 int main(void)
@@ -40,9 +60,21 @@ int main(void)
 	initPorts();
 	initTimer1();
 	sei();
-    
-    while (1) 
-    {
-    }
-}
 
+	while (1)
+	{
+		if (!(PINA & (1 << PA0)))
+		{
+			TIMSK |= (1 << TICIE1);
+			TIMSK |= (1 << TOIE0);
+			PORTC ^= (1 << PC0); // Toggle PC0
+			_delay_ms(500);      // Delay 500ms
+		}
+		else
+		{
+			TIMSK &= ~(1 << TICIE1);
+			TIMSK &= ~(1 << TOIE0);
+			PORTC |= (1 << PC0); // Set PC0 high
+		}
+	}
+}
