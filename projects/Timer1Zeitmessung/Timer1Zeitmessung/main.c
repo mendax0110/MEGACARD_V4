0867ee47e0bba528c3fb49dd0ae72780b77ec4ac
m3ndax <adrian.goessl@outlook.com>
2023-08-26 15:47:04 +0200

    updated comments

--- projects/Timer1Zeitmessung/Timer1Zeitmessung/main.c
+++ projects/Timer1Zeitmessung/Timer1Zeitmessung/main.c
@@ -19,6 +19,7 @@ long int Time = 0;
 
 volatile long int Timer1Counter = 0;
 
+// Timer1 Input Capture Interrupt
 ISR(TIMER1_CAPT_vect)
 {
 	ending_edge = 256 * ICR1H + ICR1L;
@@ -28,11 +29,13 @@ ISR(TIMER1_CAPT_vect)
 	starting_edge = ending_edge;
 }
 
+// Timer0 Overflow Interrupt
 ISR(TIMER0_OVF_vect)
 {
 	Timer1Counter++;
 }
 
+// Initialize Ports
 void initPorts()
 {
 	DDRC |= 0xFF; // portc output
@@ -48,18 +51,20 @@ void initPorts()
 	PORTC |= (1 << PC0);  // Set PC0 high initially
 }
 
+// Initialize Timer1
 void initTimer1()
 {
 	TCCR1A = 0x00;                         // normal mode
 	TCCR1B |= (1 << ICNC1) | (1 << CS11);  // prescaler 8bit, noise canceller -> 1.5Mhz -> 0.66us -> Zeiteinheit
 }
 
+// Main function
 int main(void)
 {
-	cli();
-	initPorts();
-	initTimer1();
-	sei();
+	cli();	// Disable global interrupts
+	initPorts();	// Initialize ports
+	initTimer1();	// Initialize Timer1
+	sei();	// Enable global interrupts
 
 	while (1)
 	{
