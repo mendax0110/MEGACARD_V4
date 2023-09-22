edaac26ea975df848fe654808d1445fb93f01b1d
m3ndax <adrian.goessl@outlook.com>
2023-09-22 16:20:34 +0200

    changed some comments, deleted not used code

--- projects/Timer1Zeitmessung/Timer1Zeitmessung/main.c
+++ projects/Timer1Zeitmessung/Timer1Zeitmessung/main.c
@@ -19,27 +19,24 @@ long int Time = 0;
 
 volatile long int Timer1Counter = 0;
 
-// Timer1 Input Capture Interrupt
 ISR(TIMER1_CAPT_vect)
 {
-	ending_edge = 256 * ICR1H + ICR1L;
+	ending_edge = 256 * ICR1H + ICR1L;					// also with bit shifting usable e.g: (ICR1H << 8) | ICR1L
 	clocks = ending_edge - starting_edge + Timer1Counter * 65535;
-	Time = clocks / 1500; // Timer in ms (12Mhz/8) -> 1 500 000 / 1500 -> 1000Hz, das entspricht 1 ms
+	Time = clocks / 1500;								// Timer in ms (12Mhz/8) -> 1 500 000 / 1500 -> 1000Hz, das entspricht 1 ms
 	Timer1Counter = 0;
 	starting_edge = ending_edge;
 }
 
-// Timer0 Overflow Interrupt
 ISR(TIMER0_OVF_vect)
 {
 	Timer1Counter++;
 }
 
-// Initialize Ports
 void initPorts()
 {
 	DDRC |= 0xFF; // portc output
-	PORTC = 0xFF; // all led dark
+	PORTC = 0x00; // all led dark
 
 	DDRA &= ~(1 << PA0); // for S0
 	PORTA |= (1 << PA0); // for S0
@@ -47,28 +44,27 @@ void initPorts()
 	DDRD &= ~(1 << PD6); // ICP Input Capture
 	PORTD &= ~(1 << PD6); // Hi-Z, cuz connected to PA0 (Pull up)
 	
-	DDRC |= (1 << PC0);   // Set PC0 as output
-	PORTC |= (1 << PC0);  // Set PC0 high initially
+	//DDRC |= (1 << PC0);   // Set PC0 as output
+	//PORTC |= (1 << PC0);  // Set PC0 high initially
 }
 
-// Initialize Timer1
 void initTimer1()
 {
-	TCCR1A = 0x00;                         // normal mode
-	TCCR1B |= (1 << ICNC1) | (1 << CS11);  // prescaler 8bit, noise canceller -> 1.5Mhz -> 0.66us -> Zeiteinheit
+	TCCR1A = 0x00;							// normal mode
+	TCCR1B |= (1 << ICNC1) | (1 << CS11);	// prescaler 8bit, noise canceller -> 1.5Mhz -> 0.66us -> Zeiteinheit
+	TIMSK |= (1 << TICIE1) | (1 << TOIE1);	// Capture Interrupt enable, Overflow Interrupt enable
 }
 
-// Main function
 int main(void)
 {
-	cli();	// Disable global interrupts
-	initPorts();	// Initialize ports
-	initTimer1();	// Initialize Timer1
-	sei();	// Enable global interrupts
+	cli();
+	initPorts();
+	initTimer1();
+	sei();
 
 	while (1)
 	{
-		if (!(PINA & (1 << PA0)))
+		/*if (!(PINA & (1 << PA0)))
 		{
 			TIMSK |= (1 << TICIE1);
 			TIMSK |= (1 << TOIE0);
@@ -80,6 +76,6 @@ int main(void)
 			TIMSK &= ~(1 << TICIE1);
 			TIMSK &= ~(1 << TOIE0);
 			PORTC |= (1 << PC0); // Set PC0 high
-		}
+		}*/
 	}
 }
